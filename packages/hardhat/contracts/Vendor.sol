96b4031253ff3296ee03faab3b4aaf87c2ff938f
ciprianhotea <cipryy_13@yahoo.com>
2022-03-22 12:20:02 +0200

    Check for balances

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -6,33 +6,37 @@ import "./YourToken.sol";
 
 contract Vendor is Ownable {
 
+  YourToken public yourToken;
   uint256 public constant tokensPerEth = 100;
 
   event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
-
-  YourToken public yourToken;
 
   constructor(address tokenAddress) {
     yourToken = YourToken(tokenAddress);
   }
 
   function buyTokens() external payable {
-    require(msg.value > 0, "ETH ammount must be greater than 0");
+    require(msg.value > 0, "ETH ammount must be greater than 0 to buy");
 
-    uint256 amountOfTokens = msg.value * tokensPerEth;
+    uint256 amountOfTokensToBuy = msg.value * tokensPerEth;
 
-    (bool sent) = yourToken.transfer(msg.sender, amountOfTokens);
-    require(sent, "Token transfer failed");
+    uint256 vendorBalance = yourToken.balanceOf(address(this));
+    require(vendorBalance >= amountOfTokensToBuy, "Vendor doesn't have enough tokens to buy");
 
-    emit BuyTokens(msg.sender, msg.value, amountOfTokens);
+    (bool sent) = yourToken.transfer(msg.sender, amountOfTokensToBuy);
+    require(sent, "Token buy failed");
+
+    emit BuyTokens(msg.sender, msg.value, amountOfTokensToBuy);
   }
 
-  function sellTokens(uint256 amountOfTokens) external {
-    require(amountOfTokens > 0, "Tokens amount must be greater than 0");
+  function sellTokens(uint256 amountOfTokensToSell) external {
+    require(amountOfTokensToSell > 0, "Tokens amount must be greater than 0 to sell");
 
-    uint256 amountOfEth = amountOfTokens / tokensPerEth;
+    uint256 senderBalance = yourToken.balanceOf(msg.sender);
+    require(senderBalance >= amountOfTokensToSell, "Your balance doesn't have enough tokens to sell");
 
-    (bool sent) = yourToken.transferFrom(msg.sender, address(this), amountOfTokens);
+    uint256 amountOfEth = amountOfTokensToSell / tokensPerEth;
+    (bool sent) = yourToken.transferFrom(msg.sender, address(this), amountOfTokensToSell);
     require(sent, "Token transfer failed");
 
     (sent,) = msg.sender.call{value: amountOfEth}("");
@@ -40,7 +44,10 @@ contract Vendor is Ownable {
   }
 
   function withdraw() external onlyOwner {
+    uint256 ownerBalance = address(this).balance;
+    require(ownerBalance > 0, "Owner has no balance to withdraw");
+
     (bool sent,) = msg.sender.call{value: address(this).balance}("");
-    require(sent, "Failed to withdraw");
+    require(sent, "Failed to withdraw owner balance");
   }
 }
