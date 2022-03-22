bfc009f7064e3b7b67ea3fb640101f5b79d29893
ciprianhotea <cipryy_13@yahoo.com>
2022-03-22 12:09:24 +0200

    Implement Vendor contract

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -6,7 +6,9 @@ import "./YourToken.sol";
 
 contract Vendor is Ownable {
 
-  //event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+  uint256 public constant tokensPerEth = 100;
+
+  event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
 
   YourToken public yourToken;
 
@@ -14,10 +16,31 @@ contract Vendor is Ownable {
     yourToken = YourToken(tokenAddress);
   }
 
-  // ToDo: create a payable buyTokens() function:
+  function buyTokens() external payable {
+    require(msg.value > 0, "ETH ammount must be greater than 0");
 
-  // ToDo: create a withdraw() function that lets the owner withdraw ETH
+    uint256 amountOfTokens = msg.value * tokensPerEth;
 
-  // ToDo: create a sellTokens() function:
+    (bool sent) = yourToken.transfer(msg.sender, amountOfTokens);
+    require(sent, "Token transfer failed");
 
+    emit BuyTokens(msg.sender, msg.value, amountOfTokens);
+  }
+
+  function sellTokens(uint256 amountOfTokens) external {
+    require(amountOfTokens > 0, "Tokens amount must be greater than 0");
+
+    uint256 amountOfEth = amountOfTokens / tokensPerEth;
+
+    (bool sent) = yourToken.transferFrom(msg.sender, address(this), amountOfTokens);
+    require(sent, "Token transfer failed");
+
+    (sent,) = msg.sender.call{value: amountOfEth}("");
+    require(sent, "Eth send failed");
+  }
+
+  function withdraw() external onlyOwner {
+    (bool sent,) = msg.sender.call{value: address(this).balance}("");
+    require(sent, "Failed to withdraw");
+  }
 }
